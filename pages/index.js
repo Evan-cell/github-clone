5ef4e1ad98031d805e395cc6817b4d6ec70cd45a
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-10-20 11:57:39 +0300

    github-clone

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div>
+      <div className="relative z-50">
+        <div className="absolute"></div>
+      </div>
+      <div>
+        <div  className="overflow-hidden">
+          <div className="relative">
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" alt="" />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
